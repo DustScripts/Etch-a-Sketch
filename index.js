4b869ed38283637ec8bab135c81c522baabac710
Dustrider <34176847+DustScripts@users.noreply.github.com>
2024-05-15 11:15:02 -0400

    working on grid, got something sort of working but bottom row is showing off and I think it's because of the calculation of the flexBasis

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const container = document.getElementById("container");
 
-for (i = 0; i < 256; i++) {
+const ROWS_COLUMNS = 64;
+
+const numberOfSquares = ROWS_COLUMNS * ROWS_COLUMNS;
+console.log(numberOfSquares);
+
+for (i = 0; i < numberOfSquares; i++) {
   let gridDiv = document.createElement("div");
-  gridDiv.classList.add("divGrid");
-  gridDiv.textContent = `${i}`;
+  gridDiv.classList.add("divGrid", i);
+  // gridDiv.style.flexBasis = Math.floor(100 / ROWS_COLUMNS) + "%";
+  gridDiv.style.flexBasis = 100 / ROWS_COLUMNS + "%";
+  // gridDiv.textContent = `${i}`;
   container.appendChild(gridDiv);
-  console.log(gridDiv);
+  // console.log(gridDiv);
 }
